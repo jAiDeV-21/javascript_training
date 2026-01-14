6cf79a106b6ab2f76a0093fcccd93551cb9ebe35
jaidev-21 <jaidevs2107@gmail.com>
2026-01-14 22:15:07 +0530

    Complete Assignment-3

--- control_flow_functions.js
+++ control_flow_functions.js
@@ -1,4 +1,5 @@
 /* Assignment - 1 : Create a program to check whether a number is positive, negative or zero. */
+console.log("************** Assignment - 1 **************");
 let num = 1;
 // let num = 0;
 // let num = -1;
@@ -9,8 +10,34 @@ else { console.log("The number is negative."); }
 
 
 /* Assignment - 2 : Create a program to check whether the number in range 1 to 20 are even or odd. */
+console.log("************** Assignment - 2 **************");
 for (let i = 1; i <= 20; i++)
 {
     if (i & 1) { console.log(`${i} is odd.`); }
     else { console.log(`${i} is even.`); }
+}
+
+
+/* Assignment - 3 : Create a program to give role based access. */
+console.log("************** Assignment - 3 **************");
+let role = "ADMIN";
+// let role = "USER";
+// let role = "MANAGER";
+
+switch (role)
+{
+    case "ADMIN":
+        console.log("Full access.");
+        break;
+    
+    case "MANAGER":
+        console.log("Moderate access.");
+        break;
+
+    case "USER":
+        console.log("Limited access.");
+        break;
+
+    default:
+        console.log("Invalid role.");
 }
